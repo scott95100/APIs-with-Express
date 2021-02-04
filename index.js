c49048420111c6de354977db5159924f5a4e6c37
scott95100 <scott95100@gmail.com>
2021-02-04 13:07:44 -0700

    index: got search function running

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
 
@@ -15,13 +16,48 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded( {extended: false}))
 app.use(methodOverride('_method'))
 
-////////ROUTS/////////////
+////////ROUTES/////////////
+//test for route running
 app.get('/', (req, res)=> {
     res.send('hello')
 })
 
+app.get('/omdb', (req, res)=> {
+    const qs= {
+        params: {
+            s: 'star wars',
+            apikey: process.env.API_KEY
+        }
+    }
+    axios.get('http://www.omdbapi.com', qs)
+    .then((response)=> {
+        
+        
+    })
+})
+
+//with Swapi api
+app.get('/swapi/search', (req, res)=> {
+    res.render('search')
+})
+
+app.get('/swapi/show', (req, res)=> {
+    console.log('made it here')
+    console.log('query', req.query)
+    axios.get(`http://swapi.dev/api/people/${req.query.personId}`)
+    .then((response)=> {
+        console.log(response.data)
+        //make a person object
+        const person = {
+            name: response.data.name,
+            birth: response.data['birth_year'],
+            home: response.data.homeworld
+        }
+        res.render('show', person)
+    })
+})
 
 
-
+/////////SERVER PORT/////////
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Listening on ${PORT}`))
